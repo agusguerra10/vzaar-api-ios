1fc0221f4a1f176e70c69657d0dd73384d601010
andreasbagias <andreasbagias@gmail.com>
2017-07-18 14:59:34 +0100

    uploader public to private

--- Vzaar/VzaarCreateLinkUploadParameters.swift
+++ Vzaar/VzaarCreateLinkUploadParameters.swift
@@ -12,8 +12,8 @@ public class VzaarCreateLinkUploadParameters: VzaarRequestParameters{
     
     //Url to video file. - REQUIRED
     public var url: String?
-    //Uploader description used for metadata, analytics and support - REQUIRED
-    public var uploader: String?
+    //REQUIRED (HARDCODED) uploader description used for metadata, analytics and support
+    private var uploader: String = "ios-sdk-1.0.1"
     //Specify if you do not want to use your default Ingest Recipe.
     public var ingest_recipe_id: NSNumber?/*objective-C*/
     //Video title. If not provided this will default to your source filename.
@@ -37,7 +37,7 @@ public class VzaarCreateLinkUploadParameters: VzaarRequestParameters{
         var params: [String: Any] = [String: Any]()
         
         if let url = url{ params["url"] = url }
-        if let uploader = uploader{ params["uploader"] = uploader }
+        params["uploader"] = uploader
         if let ingest_recipe_id = ingest_recipe_id{ params["ingest_recipe_id"] = ingest_recipe_id }
         if let title = title{ params["title"] = title }
         if let linkDescription = linkDescription{ params["description"] = linkDescription }
